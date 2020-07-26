c43d0f3f7cb1708a25a76a10facc6dd2ac726665
Nnamdi <nnwabuokei@paga.com>
2020-07-26 14:28:12 +0100

    Create a greet and main package

---
+++ gopherguides/greet/greet.go
@@ -0,0 +1,10 @@
+//https://www.digitalocean.com/community/tutorials/how-to-install-go-and-set-up-a-local-programming-environment-on-macos
+
+package greet
+
+import "fmt"
+
+//Hello prints Hello World
+func Hello()  {
+	fmt.Println("Hello, World!")
+}
