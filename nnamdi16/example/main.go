dfe03545ea266b28c8de06ea2d09b36054e78044
Nnamdi <nnwabuokei@paga.com>
2020-07-26 15:33:30 +0100

    Accessed variables  and struct in greet package

--- nnamdi16/example/main.go
+++ nnamdi16/example/main.go
@@ -1,6 +1,20 @@
 package main
-import "github.com/nnamdi16/Go-training/nnamdi16/greet"
+import (
+	"github.com/nnamdi16/Go-training/nnamdi16/greet"
+	"fmt"
+)
 
 func main()  {
 	greet.Hello();
+	fmt.Println(greet.Shark)
+
+	oct:= greet.Octopus{
+		Name:"Jesse",
+		Color:"orange",
+	}
+
+	fmt.Println(oct.String())
+	fmt.Println(oct.Name)
+	oct.Reset()
+	fmt.Println(oct.String())
 }
