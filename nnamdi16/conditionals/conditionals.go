17cea1497ddb5f857bb1fb8313f0789fc2483438
Nnamdi <nnwabuokei@paga.com>
2020-07-29 14:59:00 +0100

    Implemented if/else and switch case

---
+++ nnamdi16/conditionals/conditionals.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+func switchCase()  {
+	galaxy := "M87"
+	switch galaxy {
+	case "Milky Way":
+		fmt.Printf("Galaxy name is 'Milky Way' \n")
+	case "Andromeda":
+		fmt.Printf("Galaxy name is 'Andromedia'\n")
+	case "M87":
+		fmt.Printf("Galaxy name is 'M87'\n")
+	}
+}
+
+func main()  {
+	a, b := 1,10
+
+	if a < b {
+		fmt.Printf("%d is less than %d\n", a,b)
+	} else if a == b {
+		fmt.Printf("%d is equal to %d\n", a, b)
+	} else {
+		fmt.Printf("%d is greater than %d\n", a, b)
+	}
+
+	switchCase()
+}
