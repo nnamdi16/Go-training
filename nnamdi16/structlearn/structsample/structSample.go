9ee96feca7f6c6b558e0b932168cecfa3150d23e
Nnamdi <nnwabuokei@paga.com>
2020-08-14 13:08:18 +0100

    Created a Person struct type

---
+++ nnamdi16/structlearn/structsample/structSample.go
@@ -0,0 +1,14 @@
+package structsample
+
+
+//Person represents an Example function
+type Person struct {
+	FirstName string
+	LastName  string
+	City      string
+	Gender 	  string
+	Age  	  int
+}
+
+//Struct is a collect of fields
+//go mod init struct
